7324d2ab63c8256e6b76b3244e45995ec51aecb6
Pascal Mueller <mueller.pascal@hotmail.com>
2021-06-11 19:56:08 +0200

    added plotting

--- problem2/plot.py
+++ problem2/plot.py
@@ -1,6 +1,8 @@
+import os
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import struct
 
 def getParallelEfficency(results):
     T1 = int(float(results[0][1])) # s
@@ -9,7 +11,8 @@ def getParallelEfficency(results):
     speed_up = np.zeros(len(results))
     
     for (i, result) in enumerate(results):
-        T_p = int(float(result[1])) # s
+        T_p = float(result[1]) # s
+        #print(f"{T_p} for {result}")
         speed_up[i] = T1/T_p # T1/T_p
         p_eff[i] = speed_up[i]/int(result[0]) #Speedup/p
 
@@ -21,29 +24,41 @@ N = [500, 1000, 2000, 3000]
 # Plot
 fig, (ax1, ax2) = plt.subplots(1,2)
 
-for (j,n) enumerate(N):
-    resuts = []
+for (j,n) in enumerate(N):
+    results = []
     runtimes = []
+    PP = [] 
     for (i,p) in enumerate(P):
-        with open(filename, "rb") as f:
-            buf = f.read(12)
-            # Data Layout: <int:num_threads>
-            #              <double:elapsed_time>
-            results.append(struct.unpack('=id', buf))
-        
-        runtimes.append(results[i][1])
-    speed_up, p_eff = getParallelEfficency(runtimes, P)
+        filename = f"benchmarks/benchmark_{n}_{p}.bin"
+        if os.path.exists(filename):
+            with open(filename, "rb") as f:
+                buf = f.read(12)
+                # Data Layout: <int:num_threads>
+                #              <double:elapsed_time>
+                data = struct.unpack('=id', buf)
+                data = np.array([data[0], data[1]])
+                results.append(data)
+                runtimes.append(data[1])
+                PP.append(p)
+        else:
+            print("file missing:", filename)
+
+    speed_up, p_eff = getParallelEfficency(results)
+
 
     # Plot runtimes
-    runtimes = results
-    ax1.plot(P, runtimes)
+    ax1.plot(PP, runtimes, '-o', label=f"N={n}")
+    ax1.set_xticks(PP)
     ax1.invert_yaxis()
     ax1.set(xlabel='Number of threads [-]', ylabel='Runtime [s]', title='Runtimes')
 
     # Plotspeed_up 
-    ax2.plot(P, speed_up)
+    ax2.plot(PP, speed_up, '-o', label=f"N={n}")
+    ax1.set_xticks(PP)
     ax2.set(xlabel='Number of threads [-]', ylabel='Strong Speedup [-]', title='Strong scaling')
 
+ax1.legend()
+ax2.legend()
 plt.tight_layout()
     
 fig.savefig(f"strong_scaling.png")
